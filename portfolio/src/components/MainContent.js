fd9a24088f1c9a22fa1c14a47f6d311be3086ae4
seru01 <reynaldbartiana2023@gmail.com>
2024-02-08 21:04:42 +0800

    test commit

--- portfolio/src/components/MainContent.js
+++ portfolio/src/components/MainContent.js
@@ -3,8 +3,9 @@ import React from "react";
 const Maincontent = () => {
   return (
     <div className="MainContent">
-      <h3>Reynald Bartiana</h3>
-      <p>Software Developer / Firmware Engineer</p>
+      <div className="">
+
+      </div>
     </div>
   );
 };
