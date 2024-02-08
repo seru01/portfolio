7b88b1d211409bde5044be527257f4188390eec8
seru01 <reynaldbartiana2023@gmail.com>
2024-02-08 23:18:49 +0800

    main content profile done

--- portfolio/src/components/MainContent.js
+++ portfolio/src/components/MainContent.js
@@ -3,8 +3,24 @@ import React from "react";
 const Maincontent = () => {
   return (
     <div className="MainContent">
-      <div className="">
+      <div class="triangle-down"></div>
+      <div class="circle"></div>
+      <div class="square"></div>
 
+      <div className="Card Profile">
+        <div className="imageSection"></div>
+        <div className="infoSectiom">
+          <div className="infoName">
+            <h2>Reynald Bartiana</h2>
+          </div>
+          <div className="infoTitle">
+            <h4>Software Developer / Firmware Engineer</h4>
+          </div>
+          <div className="infoProgLang"></div>
+          <div className="infoDescription">
+            <p>lorem ipsum</p>
+          </div>
+        </div>
       </div>
     </div>
   );
